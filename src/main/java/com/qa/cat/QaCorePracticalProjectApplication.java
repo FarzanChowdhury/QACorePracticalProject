7ead5f5eed2139b7a0ab7be0c655a613144865ab
Farzan Chowdhury <farzan.chowdhuryy@gmail.com>
2022-03-04 01:12:23 +0000

    project commit

---
+++ src/main/java/com/qa/cat/QaCorePracticalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaCorePracticalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaCorePracticalProjectApplication.class, args);
+	}
+
+}
